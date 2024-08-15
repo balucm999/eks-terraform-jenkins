50e8ff4b570806010590030f30ad9d8eb8718812
balucm999 <balamuralikrishna@emmsonsinfotech.com>
2024-08-15 22:35:55 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -69,29 +69,29 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
 }
 
 # OIDC
-resource "aws_iam_role" "eks_oidc" {
-  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
-  name               = "eks-oidc"
-}
+//resource "aws_iam_role" "eks_oidc" {
+  //assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
+  //name               = "eks-oidc"
+//}
 
-resource "aws_iam_policy" "eks-oidc-policy" {
-  name = "test-policy"
+//resource "aws_iam_policy" "eks-oidc-policy" {
+  //name = "test-policy"
 
-  policy = jsonencode({
-    Statement = [{
-      Action = [
-        "s3:ListAllMyBuckets",
-        "s3:GetBucketLocation",
-        "*"
-      ]
-      Effect   = "Allow"
-      Resource = "*"
-    }]
-    Version = "2012-10-17"
-  })
-}
+  //policy = jsonencode({
+    //Statement = [{
+      //Action = [
+        //"s3:ListAllMyBuckets",
+        //"s3:GetBucketLocation",
+        //"*"
+      //]
+      //Effect   = "Allow"
+      //Resource = "*"
+    //}]
+    //Version = "2012-10-17"
+  //})
+//}
 
-resource "aws_iam_role_policy_attachment" "eks-oidc-policy-attach" {
-  role       = aws_iam_role.eks_oidc.name
-  policy_arn = aws_iam_policy.eks-oidc-policy.arn
-}
+//resource "aws_iam_role_policy_attachment" "eks-oidc-policy-attach" {
+  //role       = aws_iam_role.eks_oidc.name
+  //policy_arn = aws_iam_policy.eks-oidc-policy.arn
+//}

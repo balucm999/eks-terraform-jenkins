a79b5a7a77c0b9d22434a1fed0eefd7119bf4474
balucm999 <balamuralikrishna@emmsonsinfotech.com>
2024-08-15 18:07:28 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -48,9 +48,9 @@ module "eks" {
   }
   tags = local.tags
 }
-# OIDC Provider
-resource "aws_iam_openid_connect_provider" "eks-oidc" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
-  url             = data.tls_certificate.eks-certificate.url
-}
+//# OIDC Provider
+//resource "aws_iam_openid_connect_provider" "eks-oidc" {
+ // client_id_list  = ["sts.amazonaws.com"]
+  //thumbprint_list = [data.tls_certificate.eks-certificate.certificates[0].sha1_fingerprint]
+  //url             = data.tls_certificate.eks-certificate.url
+//}

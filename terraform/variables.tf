008c0411c65c81643183c7ed904e331791295999
balucm999 <balamuralikrishna@emmsonsinfotech.com>
2024-08-15 22:46:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,37 +18,37 @@ variable "pri-cidr-block" {
 variable "pri-availability-zone" {
   type = list(string)
 }
-variable "pri-sub-name" {}
-variable "public-rt-name" {}
-variable "private-rt-name" {}
-variable "eip-name" {}
-variable "ngw-name" {}
-variable "eks-sg" {}
+//variable "pri-sub-name" {}
+//variable "public-rt-name" {}
+//variable "private-rt-name" {}
+//variable "eip-name" {}
+//variable "ngw-name" {}
+//variable "eks-sg" {}
 
 #IAM
-variable "is_eks_role_enabled" {
-  type = bool
-}
-variable "is_eks_nodegroup_role_enabled" {
-  type = bool
-}
+//variable "is_eks_role_enabled" {
+  //type = bool
+//}
+//variable "is_eks_nodegroup_role_enabled" {
+  //type = bool
+//}
 
 # EKS
-variable "is-eks-cluster-enabled" {}
-variable "cluster-version" {}
-variable "endpoint-private-access" {}
-variable "endpoint-public-access" {}
-variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
-}
-variable "ondemand_instance_types" {}
-variable "spot_instance_types" {}
-variable "desired_capacity_on_demand" {}
-variable "min_capacity_on_demand" {}
-variable "max_capacity_on_demand" {}
-variable "desired_capacity_spot" {}
-variable "min_capacity_spot" {}
-variable "max_capacity_spot" {}
+//variable "is-eks-cluster-enabled" {}
+//variable "cluster-version" {}
+//variable "endpoint-private-access" {}
+//variable "endpoint-public-access" {}
+//variable "addons" {
+  //type = list(object({
+    //name    = string
+    //version = string
+  //}))
+//}
+//variable "ondemand_instance_types" {}
+//variable "spot_instance_types" {}
+//variable "desired_capacity_on_demand" {}
+//variable "min_capacity_on_demand" {}
+//variable "max_capacity_on_demand" {}
+//variable "desired_capacity_spot" {}
+//variable "min_capacity_spot" {}
+//variable "max_capacity_spot" {}
